3aaec6d551a8e5a9f30c50fbbe9ac2e88eefdbcd
한종민 <jongm0203@gmail.com>
2026-01-20 11:22:16 +0900

    chore(build): 릴리즈 빌드 시 release signingConfig 사용하도록 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 
     buildTypes {
         getByName("release") {
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
 
         create("benchmark") {
